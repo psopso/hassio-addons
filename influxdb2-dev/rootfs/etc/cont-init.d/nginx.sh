5b2f666fe9fa96def3a7cb4411aea97f577b4e47
psopso <57403373+psopso@users.noreply.github.com>
2023-09-20 20:35:53 +0200

    Update nginx.sh

--- influxdb2-dev/rootfs/etc/cont-init.d/nginx.sh
+++ influxdb2-dev/rootfs/etc/cont-init.d/nginx.sh
@@ -33,8 +33,8 @@ fi
 
 ingress_port=$(bashio::addon.ingress_port)
 ingress_interface=$(bashio::addon.ip_address)
-sed -i "s/%%port%%/${ingress_port}/g" /etc/nginx/nginx.conf
-sed -i "s/%%interface%%/${ingress_interface}/g" /etc/nginx/nginx.conf
+sed -i "s/%%port%%/${ingress_port}/g" /etc/nginx/servers/ingress.conf
+sed -i "s/%%interface%%/${ingress_interface}/g" /etc/nginx/servers/ingress.conf
 sed -i "s#%%ingress_entry%%#${ingress_entry}#g" /etc/nginx/servers/ingress.conf
 
 dns_host=$(bashio::dns.host)
