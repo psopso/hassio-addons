7d50fa9cefe13c9bc307bb586dc4de4b68518d4c
psopso <57403373+psopso@users.noreply.github.com>
2026-02-06 09:34:03 +0100

    Update x728_manager_gpio.py

--- x728_gpio_button_manager/x728_manager_gpio.py
+++ x728_gpio_button_manager/x728_manager_gpio.py
@@ -77,11 +77,11 @@ def main():
                 last_check = now
                 try:
                     voltage = read_voltage(bus)
-                    print(f"[X728] Napeti baterie: {voltage} V", flush=True)
+                    #print(f"[X728] Napeti baterie: {voltage} V", flush=True)
 
                     if voltage <= LOW_VOLTAGE and not shutdown_sent:
                         print("[X728] Nizke napeti -> SHUTDOWN", flush=True)
-                        lines.set_value(PIN_ENABLE, Value.INACTIVE)
+                        #lines.set_value(PIN_ENABLE, Value.INACTIVE)
                         run_command("shutdown")
                         shutdown_sent = True
                 except Exception as e:
