b173ac2b93f1f9b95aabf937bb5e83c7f07a2e45
psopso <57403373+psopso@users.noreply.github.com>
2026-02-20 14:36:26 +0100

    environment

--- x728_gpio_button_manager/run.sh
+++ x728_gpio_button_manager/run.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/with-contenv bashio
 set -e
 
+echo mqttuser
 echo $MQTT_USERNAME
 
-python3 -u /x728_manager_gpio.py $__BASHIO_SUPERVISOR_TOKEN
+#python3 -u /x728_manager_gpio.py $__BASHIO_SUPERVISOR_TOKEN
