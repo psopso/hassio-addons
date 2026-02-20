46ec2577d6fe1c164ed45847420f392af449ea51
psopso <57403373+psopso@users.noreply.github.com>
2026-02-20 16:27:30 +0100

    Update run.sh

--- x728_gpio_button_manager/run.sh
+++ x728_gpio_button_manager/run.sh
@@ -2,12 +2,12 @@
 set -e
 
 # Načtení údajů z MQTT service discovery
-MQTT_HOST=$(bashio::services mqtt "host")
-MQTT_PORT=$(bashio::services mqtt "port")
-MQTT_USER=$(bashio::services mqtt "username")
-MQTT_PASS=$(bashio::services mqtt "password")
+export MQTT_HOST=$(bashio::services mqtt "host")
+export MQTT_PORT=$(bashio::services mqtt "port")
+export MQTT_USERNAME=$(bashio::services mqtt "username")
+export MQTT_PASSWORD=$(bashio::services mqtt "password")
 
 # Příklad výpisu (jen pro ladění, v produkci heslo nevypisujte)
-bashio::log.info "Connecting to MQTT at ${MQTT_HOST}:${MQTT_PORT}"
+# bashio::log.info "Connecting to MQTT at ${MQTT_HOST}:${MQTT_PORT}"
 
-#python3 -u /x728_manager_gpio.py $__BASHIO_SUPERVISOR_TOKEN
+python3 -u /x728_manager_gpio.py $__BASHIO_SUPERVISOR_TOKEN
