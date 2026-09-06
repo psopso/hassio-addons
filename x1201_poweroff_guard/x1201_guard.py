e40a1d3bfabc6e3ef6f6133c0a67449d04fdfb74
psopso <57403373+psopso@users.noreply.github.com>
2026-09-06 17:52:57 +0200

    Update x1201_guard.py

--- x1201_poweroff_guard/x1201_guard.py
+++ x1201_poweroff_guard/x1201_guard.py
@@ -3,43 +3,54 @@ import time
 import gpiod
 from gpiod.line import Direction, Value
 
-from datetime import datetime
-
-CHIP = "/dev/gpiochip0"
+GPIO_CHIP = "/dev/gpiochip0"
 GPIO_AC = 6
 
-def shutdown(signum, frame):
+
+def handle_shutdown(signum, frame):
     print("[X1201] SIGTERM received", flush=True)
 
-#    value = lines.get_value(GPIO_AC)
+    try:
+        print("[X1201] Trying to acquire GPIO6...", flush=True)
 
-#    if value == Value.ACTIVE:
-#        print("[X1201] GPIO6 = HIGH -> AC PRESENT", flush=True)
-#    else:
-#        print("[X1201] GPIO6 = LOW -> AC ABSENT", flush=True)
+        # GPIO si vezmeme AŽ TEĎ.
+        # Za normálního provozu ho vůbec nedržíme.
+        with gpiod.request_lines(
+            GPIO_CHIP,
+            consumer="x1201-poweroff-guard",
+            config={
+                GPIO_AC: gpiod.LineSettings(
+                    direction=Direction.INPUT
+                )
+            }
+        ) as lines:
 
-#    print(f"[X1201] timestamp = {time.time()}", flush=True)
-    print(f"[X1201] timestamp = {datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]}", flush=True)
+            value = lines.get_value(GPIO_AC)
 
-    # ZATÍM NIC NEDĚLAT
-    # pouze ukončit addon
+            print(
+                f"[X1201] GPIO6 = "
+                f"{'HIGH -> AC PRESENT' if value == Value.ACTIVE else 'LOW -> AC ABSENT'}",
+                flush=True
+            )
+
+            print(
+                f"[X1201] timestamp = {time.time()}",
+                flush=True
+            )
+
+    except Exception as e:
+        print(f"[X1201] ERROR acquiring GPIO6: {e}", flush=True)
+
+    print("[X1201] Exiting", flush=True)
     raise SystemExit(0)
 
 
-signal.signal(signal.SIGTERM, shutdown)
-signal.signal(signal.SIGINT, shutdown)
+signal.signal(signal.SIGTERM, handle_shutdown)
+signal.signal(signal.SIGINT, handle_shutdown)
 
-#with gpiod.request_lines(
-#    CHIP,
-#    consumer="x1201-poweroff-guard",
-#    config={
-#        GPIO_AC: gpiod.LineSettings(
-#            direction=Direction.INPUT
-#        )
-#    }
-#) as lines:
-if True:
-    print("[X1201] Poweroff Guard started", flush=True)
+print("[X1201] Guard started", flush=True)
 
-    while True:
-        time.sleep(3600)
+# GPIO6 zde NENÍ otevřené.
+# Pouze čekáme na ukončení kontejneru.
+while True:
+    time.sleep(3600)
